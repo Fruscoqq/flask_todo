41b00d218a2b5b20b340cfb5f5d81bb36d5a5489
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-11-03 21:37:53 +0000

    initial commit

---
+++ application/routes.py
@@ -0,0 +1,37 @@
+from application import app, db
+from application.models import Tasks
+from flask import render_template, redirect, url_for, request
+
+
+@app.route('/home')
+def home():
+    tasks_list = Tasks.query.all()
+    return render_template('index.html', tasks_list=tasks_list)
+
+@app.route('/add', methods=["POST"])
+def add():
+    # add new task
+    description = request.form.get("description")
+    new_task = Tasks(description=description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route('/update/<int:task_id>', methods=['POST', 'GET'])
+def update(task_id):
+    task = Tasks.query.get(task_id)
+    if request.method == 'POST':
+        task.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('update.html', task=task)
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    # delete task
+    task = Tasks.query.filter_by(id=task_id).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
