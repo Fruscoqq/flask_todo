c6a4b3f9ce8806c398f02635c0ea7155938f0293
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-11-15 16:12:49 +0000

    updating files

--- application/routes.py
+++ application/routes.py
@@ -3,11 +3,12 @@ from application.models import Tasks
 from flask import render_template, redirect, url_for, request
 
 
-@app.route('/home')
-def home():
+@app.route('/')
+def index():
     tasks_list = Tasks.query.all()
     return render_template('index.html', tasks_list=tasks_list)
 
+# Add route
 @app.route('/add', methods=["POST"])
 def add():
     # add new task
@@ -15,23 +16,25 @@ def add():
     new_task = Tasks(description=description)
     db.session.add(new_task)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
+# Update Route
 @app.route('/update/<int:task_id>', methods=['POST', 'GET'])
 def update(task_id):
     task = Tasks.query.get(task_id)
     if request.method == 'POST':
         task.description = request.form['description']
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('index'))
     else:
         return render_template('update.html', task=task)
 
+# Delete route
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     # delete task
     task = Tasks.query.filter_by(id=task_id).first()
     db.session.delete(task)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
