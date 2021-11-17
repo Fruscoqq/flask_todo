8f8391566f9005bf7f0a784fc2d0e7231fb179de
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-11-17 16:23:07 +0000

    added modal to delete, small design changes

--- application/__init__.py
+++ application/__init__.py
@@ -9,8 +9,9 @@ assets.url = app.static_url_path
 scss = Bundle('styles/style.scss', filters='pyscss', output='styles/style.css')
 assets.register('scss_all', scss)
 
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+app.secret_key = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 
 db = SQLAlchemy(app)
