ace65f2f36f3f49a66c4bb7109a95726a8c13365
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-11-23 16:13:34 +0000

    saving progress

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -1,51 +1,45 @@
 from application import app, db
 from application.models import Tasks
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, jsonify
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     tasks_list = Tasks.query.all()
-    return render_template('index.html', tasks_list=tasks_list)
+    tasks_dict = {"tasks":[]}
+    for task in tasks_list:
+        tasks_dict["tasks"].append(
+            {
+                "id": task.id,
+                "description": task.description,
+                "laptime": task.laptime
+            }
+        )
+    return jsonify(tasks_dict)
 
 # Add route
 @app.route('/add', methods=["POST"])
 def add():
     # add new task
-    description = request.form.get("description")
-    laptime = request.form.get("laptime")
-    if description == "":
-        flash("Something went wrong", "newWarning")
-        return redirect(url_for("index"))
-    else:
-        new_task = Tasks(description=description, laptime=laptime)
-        # new_laptime = Tasks(laptime=laptime)
+        new_task = Tasks(description=request.json["description"], laptime=request.json["laptime"])
         db.session.add(new_task)
-        # db.session.bulk_insert_mappings(new_task, new_laptime)
         db.session.commit()
-        flash("Your item has been added successfully", "newSuccess")
-        return redirect(url_for("index"))
+        return f"Added description {new_task.description}, laptime {new_task.laptime}"
 
 # Update Route
-@app.route('/update/<int:task_id>', methods=['POST', 'GET'])
+@app.route('/update/<int:task_id>', methods=['PUT'])
 def update(task_id):
-    task = Tasks.query.get(task_id)
-    if request.method == 'POST':
-        task.description = request.form['description']
-        task.laptime = request.form['laptime']
+        task = Tasks.query.get(task_id)
+        task.description = request.json["description"]
+        task.laptime = request.json["laptime"]
         db.session.commit()
-        return redirect(url_for('index'))
-    else:
-        flash("Your item was updated successfully", "newSuccess")
-        return render_template('update.html', task=task)
+        return f"Updated task ID {task_id} description {task.description}, laptime {task.laptime}"
 
 # Delete route
-@app.route('/delete/<int:task_id>')
+@app.route('/delete/<int:task_id>', methods=['DELETE'])
 def delete(task_id):
     # delete task
     task = Tasks.query.filter_by(id=task_id).first()
     db.session.delete(task)
     db.session.commit()
-    flash("Your item has been deleted", "newDanger")
-    return redirect(url_for("index"))
-
+    return f"Deleted task ID: {task_id} with description: {task.description} and a laptime of {task.laptime}"
