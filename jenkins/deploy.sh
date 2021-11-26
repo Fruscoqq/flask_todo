3ababbc76d1087ccc2febf0b0d31adf071df27ac
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-11-26 13:28:00 +0000

    .

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,4 @@
 
 echo "Deploy stage"
 
-ssh jenkins@dev-jenkins docker stack deploy --compose-file docker-compose.yaml trackhub
+ssh jenkins@SwarmManager docker stack deploy --compose-file docker-compose.yaml trackhub
