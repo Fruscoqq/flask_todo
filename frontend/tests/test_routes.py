e22507a6cb60c2708f2363712ff492e01b568009
Ubuntu <arvydaskel@development-server.u02kv25ig2qehmmlh4pcerxjxf.zx.internal.cloudapp.net>
2021-12-01 12:17:52 +0000

    adding test script

--- frontend/tests/test_routes.py
+++ frontend/tests/test_routes.py
@@ -1,41 +1,71 @@
 from flask import url_for
 from flask_testing import TestCase
+from application import app
 
-from application import app, db
-from application.models import Tasks
-
-# Create the base class
 class TestBase(TestCase):
-    def create_app(self):
 
-        # Pass in testing configurations for the app. 
-        # Here we use sqlite without a persistent database for our tests.
-        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
-                SECRET_KEY='TEST_SECRET_KEY',
-                DEBUG=True,
-                WTF_CSRF_ENABLED=False
-                )
+    def create_app(self):
+        # Defines the flask object's configuration for the unit tests
+        app.config.update(
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+        )
         return app
 
-        # Will be called before every test
-    def setUp(self):
-        # Creating table
-        db.create_all()
-        # Creating task object
-        demo = Tasks(description='This is test description')
-        # Saving data to db
-        db.session.add(demo)
-        db.session.commit()
-
-        # Will be called before every test
-    def tearDown(self):
-        # Closing db connection and removing all it's content
-        db.session.remove()
-        db.drop_all()
-
-# Checking if the index page opens
 class TestViews(TestBase):
-    def test_index_get(self):
-        response = self.client.get(url_for('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'This is test description', response.data)
+    # Test whether we get a successful response from our routes
+    def test_home_get(self):
+        response = self.client.get(url_for('home'))
+        self.assert200(response)
+    
+    def test_create_task_get(self):
+        response = self.client.get(url_for('create_task'))
+        self.assert200(response)
+
+    def test_read_tasks_get(self):
+        response = self.client.get(url_for('read_tasks'))
+        self.assert200(response)
+
+    def test_update_task_get(self):
+        response = self.client.get(url_for('update_task', id=1))
+        self.assert200(response)
+
+class TestRead(TestBase):
+
+    def test_read_home_tasks(self):
+        response = self.client.get(url_for('home'))
+        self.assertIn(b"Run unit tests", response.data)
+    
+    def test_read_tasks_dictionary(self):
+        response = self.client.get(url_for('read_tasks'))
+        self.assertIn(b"Run unit tests", response.data)
+
+class TestCreate(TestBase):
+
+    def test_create_task(self):
+        response = self.client.post(
+            url_for('create_task'),
+            data={"description": "Testing create functionality"},
+            follow_redirects=True
+        )
+        self.assertIn(b"Testing create functionality", response.data)
+    
+class TestUpdate(TestBase):
+
+    def test_update_task(self):
+        response = self.client.post(
+            url_for('update_task', id=1),
+            data={"description": "Testing update functionality"},
+            follow_redirects=True
+        )
+        self.assertIn(b"Testing update functionality", response.data)
+        
+
+class TestDelete(TestBase):
+
+    def test_delete_task(self):
+        response = self.client.get(
+            url_for('delete_task', id=1),
+            follow_redirects=True
+        )
+        self.assertNotIn(b"Run unit tests", response.data)
